818161b70377e891f78bd721ce8dcd2bdc63871f
ShuxinLin <linshuhsin@gmail.com>
2025-05-16 07:49:28 -0400

    added test scripts for agent_hive

---
+++ src/agent_hive/tests/multi_agent/runall.sh
@@ -0,0 +1,6 @@
+python run.py --llm 6 -f ./scenarios/multi_agent/end2end_utterance.json
+python run.py --llm 7 -f ./scenarios/multi_agent/end2end_utterance.json
+python run.py --llm 12 -f ./scenarios/multi_agent/end2end_utterance.json
+python run.py --llm 16 -f ./scenarios/multi_agent/end2end_utterance.json
+python run.py --llm 17 -f ./scenarios/multi_agent/end2end_utterance.json
+python run.py --llm 19 -f ./scenarios/multi_agent/end2end_utterance.json
